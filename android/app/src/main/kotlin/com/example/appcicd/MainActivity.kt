330de4523e54f443aced46b0573d7d7576202c2d
Rodolpho Caires <rodolpho.caires@avenuesec.io>
2023-05-23 11:51:14 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/appcicd/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.appcicd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurpleAccent.shade100,
+      appBar: AppBar(
+        title: const Text('Bitrise'),
+      ),
+      body: const SafeArea(
+        child: Center(
+          child: Text(
+            'App demo do Rod',
+            style: TextStyle(fontSize: 32),
+          ),
+        ),
+      ),
+    );
+  }
+}
